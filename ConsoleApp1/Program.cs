e9e6b282321d76ea80d1b1b7d48d77e3fe69bb9d
Your Name <you@example.com>
2022-07-13 20:57:50 +0300

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = String.Empty;
+            int age = 0;
+
+            Console.WriteLine("press any key to continue");
+
+            var response = Console.ReadKey(true).Key;
+             
+            while (response != ConsoleKey.Escape)
+            {
+                Console.WriteLine("press 1 to enter name");
+                Console.WriteLine("press 2 to enter age");
+                Console.WriteLine("press 'P' to print result");
+                Console.WriteLine("press 'Escape' to exit");
+
+                response = Console.ReadKey(true).Key;
+
+                switch (response)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            Console.WriteLine("Enter your name: ");
+                            name = Console.ReadLine();
+
+                            break;
+                        };
+                    case ConsoleKey.D2:
+                        {
+                            Console.WriteLine("Enter your age: ");
+                            string input = Console.ReadLine();
+                            bool isValid = int.TryParse(input, out var number);
+                            if (!isValid)
+                            {
+                                Console.WriteLine("Invalid input!");
+                            }
+
+                            age = number;
+                            break;
+                        }
+                    case ConsoleKey.P:
+                        {
+                            Console.WriteLine("******************");
+                            Console.WriteLine("Name: {0}", name);
+                            Console.WriteLine("Age: {0}", age);
+                            Console.WriteLine("******************");
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid menu selection!");
+
+                            break;
+                        }  
+                }
+            }          
+        }
+    }
+}
